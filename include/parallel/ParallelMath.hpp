5a5acf8ff5a6b156c8ee505211a70f1f5a445257
Boselor <xiaoyong@boselor.com>
2020-12-28 17:42:47 +0800

    提交并行组件

---
+++ include/parallel/ParallelMath.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Xiaoyong on 2020/12/28.
+//
+
+#ifndef SLC_MATH_H
+#define SLC_MATH_H
+class ParallelMath{
+
+};
+#endif //SLC_MATH_H
---
+++ src/parallel/ParallelMath.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Xiaoyong on 2020/12/28.
+//
+
+#ifndef SLC_MATH_H
+#define SLC_MATH_H
+
+#endif //SLC_MATH_H
