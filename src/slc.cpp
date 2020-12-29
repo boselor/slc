b0c68093713975bbe8ce7f9bce73d3405da7bb5b
Xiaoyong <xiaoyong@boselor.com>
2020-12-29 21:00:31 +0800

    Bug'Fix'e

--- src/slc.cpp
+++ src/slc.cpp
@@ -1,4 +1,17 @@
-//
-// Created by xiaoyong on 2020/12/29.
-//
+#include <core/Stopwatch.hpp>
+#include <core/EString.hpp>
+#include <core/IniConfigure.hpp>
+#include <log/Logger.hpp>
 
+using namespace slc;
+int main(){
+
+    Logger log;
+    log.Info(EString::format("Works start..."));
+    IniConfiguration config;
+    config.loadFile(EString::format("../app.ini"));
+    EString::format("%s",config.readString("app","wspace","").c_str()).print();
+
+    log.Info(EString::format("Works done."));
+    return 0;
+}
